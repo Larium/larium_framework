e77acebbc3fdfc17b21e3ec64adcad25510cc097
akDeveloper <andreas.kollaros@gmail.com>
2014-03-04 18:55:10 +0200

    fix config access.

--- app/Provider/AutoRenderProvider.php
+++ app/Provider/AutoRenderProvider.php
@@ -5,6 +5,7 @@
 use Larium\View\View;
 use Larium\Controller\WebHandler;
 use Larium\Controller\ActionController;
+use Larium\Http\Response;
 
 class AutoRenderProvider implements ProviderInterface
 {
@@ -15,10 +16,10 @@ class AutoRenderProvider implements ProviderInterface
     public function boot(App $app)
     {
         // Auto render action template
-        $app->registerCommand(WebHandler::AFTER_COMMAND, function($message){
+        $app->registerCommand(WebHandler::AFTER_COMMAND, function($message) use ($app){
             $command = $message->getCommand();
             if (is_array($command) && $command[0] instanceof ActionController) {
-                $view = new View($this->config->views_path);
+                $view = new View($app->getConfig()->views_path);
 
                 $actionpath = strtolower(str_replace('Controller', '',get_class($command[0])))
                     . '/'
