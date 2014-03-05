f35af9d7f6e07009aac04d90456cb69c7963dd55
akDeveloper <andreas.kollaros@gmail.com>
2014-03-05 12:23:27 +0200

    fix session registration

--- app/Provider/SessionProvider.php
+++ app/Provider/SessionProvider.php
@@ -10,16 +10,17 @@ class SessionProvider implements ProviderInterface
 {
     public function register(App $app)
     {
+        $handler = new FileSessionHandler();
+        $session = new Session($handler);
+        $app['app.session'] = $session;
     }
 
     public function boot(App $app)
     {
+        // Start session when we receive the request.
         $app->registerCommand(WebHandler::ON_REQUEST, function($message) use ($app) {
-            $handler = new FileSessionHandler();
-            $session = new Session($handler);
-            $message->getRequest()->setSession($session);
-            $session->start();
-            $app['app.session'] = $session;
+            $message->getRequest()->setSession($app['app.session']);
+            $app['app.session']->start();
         });
     }
 }
