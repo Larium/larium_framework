db5fb51b227b522aef38128b6cd7c6d9ae7b852a
akDeveloper <andreas.kollaros@gmail.com>
2014-05-01 00:54:50 +0300

    remove autoload file

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 //$start = microtime(true);
 //$memory = memory_get_usage(true);
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Larium\Http\Request;
 
